93c44beb73cd83c17d9b6b4d3feab84297aeb04a
Doppy12345 <70361052+Doppy12345@users.noreply.github.com>
2020-09-27 12:58:10 -0400

    Delete secrets.py

--- secrets.py
+++
@@ -1,8 +0,0 @@
-from wotd import emailLogin
-
-#List of all contact info for each recipiant
-
-phoneCustomers = ["7345466552@txt.att.net"]
-emailCustomers = []
-
-alertBotEmail = emailLogin("wotd.alerts@gmail.com", "qovlryhgmbqiyeqt")
